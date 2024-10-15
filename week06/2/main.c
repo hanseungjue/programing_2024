79da6d767795dc9e00bf117106954793cf8c88b8
hanseungjue <113420428+hanseungjue@users.noreply.github.com>
2024-10-16 01:48:07 +0900

    task2

---
+++ week06/2/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i;
+	printf("Enter an integer: ");
+	scanf("%i",&i);	
+	if(i < 0)
+	{
+		i=-i;
+		printf("%i",i);
+	}
+	else
+	{
+		printf("%i",i);
+	}
+	return 0;
+}
