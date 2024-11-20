84c4b8491b76dc97496cebeb5186c1c8f4e7c252
hanseungjue <113420428+hanseungjue@users.noreply.github.com>
2024-11-20 14:44:34 +0900

    week11

---
+++ week11/6/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	FILE*fp=NULL;
+	char c;
+    fp = fopen("sample.txt", "r");
+    if (fp == NULL)
+        printf("file is not open\n");
+    
+	while ((c=fgetc(fp)) != EOF)
+        putchar(c);
+
+	fclose(fp);
+	return 0;
+}
