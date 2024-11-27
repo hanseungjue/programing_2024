abfd681daec4c158f0c39bb28c4c40b59db7e4c2
hanseungjue <113420428+hanseungjue@users.noreply.github.com>
2024-11-27 14:10:59 +0900

    1

--- week13/1/main.c
+++ week13/1/main.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+// 구조체 정의
 struct student {
     int ID;
     char name[10];
     double grade;
 };
+
 int main(int argc, char *argv[]) {
-	struct student s = {24, "Eunju",4.2};
-    s.ID = 123456;
-    strcpy(s.name, "sia");
-    s.grade = 4.2;
-    printf("ID : %d\n", s.ID);
-    printf("name : %s\n", s.name);
-    printf("grade : %f\n", s.grade);
-	return 0;
+    struct student s = {24, "Eunju", 4.2};
+
+    printf("Enter ID: ");
+    scanf("%d", &s.ID);
+
+    printf("Enter name: ");
+    scanf("%s", &s.name);  
+
+    printf("Enter grade: ");
+    scanf("%lf", &s.grade); 
+
+    printf("student information - ID : %d Name : %s Grade : %f", s.ID, s.name, s.grade);
+
+    return 0;
 }
+
